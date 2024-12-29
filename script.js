1350b15837178755b0fb3d9e81b41d4babbbdf02
Naunidh26 <harnaunidh.panesar26@gmail.com>
2024-12-29 22:35:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// NAVBAR>HAMBURGER------------------------->
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navLinks.classList.toggle('active');
+    });
+});
+
+// NAVBAR>HAMBURGER END--------------------->
+
+function toggleFaq(questionElement) {
+    const answer = questionElement.nextElementSibling;
+
+    if (answer.style.display === "block") {
+        answer.style.display = "none";
+    } else {
+        answer.style.display = "block";
+    }
+}
